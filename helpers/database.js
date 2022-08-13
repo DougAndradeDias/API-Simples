0cdbe38c61a3255b725ef517e85e278495a5562c
Douglas Andrade <andrade.douglasdias@gmail.com>
2022-08-13 09:39:05 -0300

    Criado CRUD para a rota /restaurantes

--- helpers/database.js
+++ helpers/database.js
@@ -11,16 +11,23 @@ const pool = mariadb.createPool({
   connectionLimit: 5
 })
 
+// Connect and check errors
 pool.getConnection((error, connection) => {
   if (error) {
-    console.log(error.code)
+    if (error.code === 'PROTOCOL_CONNECTION_LOST') {
+      console.error('Erro de conexão com o banco de dados')
+    }
+    if (error.code === 'ERR_CON_COUNT_ERROR') {
+      console.error('Limite de conexão com o banco de dados alcançado')
+    }
+    if (error.code === 'ECONNREFUSED') {
+      console.error('Conexão com o banco de dados foi recusada')
+    }
   }
 
-  if (connection) {
-    connection.release()
-  }
+  if (connection) connection.release()
 
   return
 })
 
-// module.exports = pool
+module.exports = pool
--- routes/restaurantes.js
+++ routes/restaurantes.js
@@ -2,15 +2,75 @@ const pool = require('../helpers/database')
 const express = require('express')
 const router = express.Router()
 
-router.get('/', (request, response) => {
-  response.status(200).send('Rota funcionado')
+// Route to Add restaurant on db - CREATE
+router.post('/', async (request, response) => {
+  const { name, address } = request.body
+
+  try {
+    const createRestaurantQuery =
+      'INSERT INTO restaurantes (name, address) VALUES (?, ?)'
+    await pool.query(createRestaurantQuery, [name, address])
+
+    response.status(200).json('Inserido com sucesso')
+  } catch (error) {
+    response.status(400).json(error)
+  }
 })
 
-// Route to Add restaurant on db - CREATE
-router.post('/', (request, response) => {
-  const content = request.body
+// Route to Get restaurant list on db - READ
+router.get('/', async (request, response) => {
+  try {
+    const getRestaurantQuery = 'SELECT * FROM restaurantes'
+    const rows = await pool.query(getRestaurantQuery)
 
-  response.status(200).json(content)
+    response.status(200).json(rows)
+  } catch (error) {
+    response.status(400).json(error)
+  }
+})
+
+// Get one restaurant by id - READ
+router.get('/:id', async (request, response) => {
+  const { id } = request.params
+
+  try {
+    const getRestaurantQueryById = 'SELECT * FROM restaurantes WHERE id = ?;'
+    const rows = await pool.query(getRestaurantQueryById, id)
+
+    response.status(200).json(rows)
+  } catch (error) {
+    response.status(400).json(error)
+  }
+})
+
+// Route to Update restaurant on db - UPDATE
+router.put('/:id', async (request, response) => {
+  const { id } = request.params
+  const { name, address } = request.body
+
+  try {
+    const updateRestaurantQuery =
+      'UPDATE restaurantes SET name = ?, address = ? WHERE id = ?'
+    const rows = await pool.query(updateRestaurantQuery, [name, address, id])
+
+    response.status(200).json('Alterado com sucesso')
+  } catch (error) {
+    response.status(400).json(error)
+  }
+})
+
+// Route to Delete restaurant on db - DELETE
+router.delete('/:id', async (request, response) => {
+  const { id } = request.params
+
+  try {
+    const deleteRestaurantQuery = 'DELETE FROM restaurantes WHERE id = ?'
+    const rows = await pool.query(deleteRestaurantQuery, id)
+
+    response.status(200).json('Apagado com sucesso')
+  } catch (error) {
+    response.status(400).json(error)
+  }
 })
 
 module.exports = router
